7f5b936093200335a78e6d1bbba050f29fa661a6
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-06-14 17:42:21 +0100

    add comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// import secret key
 import { accessToken, apiKey } from "./config.js";
 
+// define main html elements
 const form = document.querySelector("form");
 const container = document.querySelector(".container");
-console.log(container);
 
+// define root and different queries
 const rootUrl = "https://api.themoviedb.org/3/trending/";
 const queries = {
   movie: "movie/",
@@ -11,8 +13,10 @@ const queries = {
   tv: "tv/",
 };
 
+// temp
 const result = [];
 
+// Event listener when form is submitted
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   // Get radio value
@@ -21,6 +25,7 @@ form.addEventListener("submit", (event) => {
   // Get dropdown value
   const time = document.querySelector("#dropdown").value;
 
+  //
   const options = {
     method: "GET",
     headers: {
@@ -30,10 +35,15 @@ form.addEventListener("submit", (event) => {
   };
 
   fetch(`${rootUrl}${queries[option]}${time}`, options)
+    // get body in json format
     .then((response) => response.json())
     .then((data) => {
+      // create an array from the data
       const resultArray = data.results;
       console.log(option);
+      //    reset the div container everytime the form is submitted
+      container.innerHTML = "";
+      //   iterate through the elements and display them
       resultArray.forEach((element) => {
         const paragraph = document.createElement("p");
         if (option == "movie") {
