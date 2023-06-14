deb654d974bd553c954eb30f09f8beea05ce0211
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-06-14 17:13:16 +0100

    eventlistener added

--- script.js
+++ script.js
@@ -8,15 +8,33 @@ const queries = {
   tv: "tv/",
 };
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  },
-};
+const result = [];
 
-fetch("https://api.themoviedb.org/3/trending/all/week", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // Get radio value
+  const option = document.querySelector('input[name="option"]:checked').value;
+
+  // Get dropdown value
+  const time = document.querySelector("#dropdown").value;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  fetch(`${rootUrl}${queries[option]}${time}`, options)
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .then((res) => console.log(res))
+    //   .then((data) => {
+    //     for(let i = 0; i < data.lenght; i++) {
+    //         console.log()
+    //     }
+    //   })
+
+    .catch((err) => console.error(err));
+});
